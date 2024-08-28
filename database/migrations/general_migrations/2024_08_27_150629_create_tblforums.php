0bd4c6b3e066b3f939ee31f9867250033efa687f
JM <wamportnapapel@gmail.com>
2024-08-28 14:42:17 +0800

    new

--- database/migrations/general_migrations/2024_08_27_150629_create_tblforums.php
+++ database/migrations/general_migrations/2024_08_27_150629_create_tblforums.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('tblforums', function (Blueprint $table) {
             $table->id();
-            $table->string('forum_id');
             $table->string('forum_name');
             $table->string('forum_desc');
             $table->string('mem_count');
