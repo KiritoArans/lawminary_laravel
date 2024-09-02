fcb0fd8736ab1680faffff1b748f9803107c68b0
larsdesu <larsenatienza917@gmail.com>
2024-09-02 17:08:13 +0800

    fast flip

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,4 +36,60 @@ class AuthController extends Controller
         }
         return redirect()->back()->withErrors(['loginError' => 'Invalid username or password.'])->withInput();
     }
+    
+    public function loginAdMod(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = UserAccount::where('username', $credentials['username'])->first();
+    
+        if ($user) {
+            if (Hash::check($credentials['password'], $user->password)) {
+                Auth::login($user);
+    
+                if ($request->has('is_moderator')) {
+                    if ($user->accountType === 'Moderator') {
+                        return redirect()->route('moderator.dashboard');
+                    } else {
+                        Auth::logout();
+                        return redirect()->back()->withErrors(['loginError' => 'Unauthorized access as Moderator.'])->withInput();
+                    }
+                } else { 
+                    if ($user->accountType === 'Admin') {
+                        return redirect()->route('admin.dashboard');
+                    } else {
+                        Auth::logout();
+                        return redirect()->back()->withErrors(['loginError' => 'Unauthorized access as Admin.'])->withInput();
+                    }
+                }
+                
+            } else {
+                if ($user->password === md5($credentials['password'])) {
+                    $user->password = Hash::make($credentials['password']);
+                    $user->save();
+                    Auth::login($user);
+    
+                    if ($request->has('is_moderator')) {
+                        if ($user->accountType === 'Moderator' || 'moderator') {
+                            return redirect()->route('moderator.dashboard');
+                        } else {
+                            Auth::logout();
+                            return redirect()->back()->withErrors(['loginError' => 'Unauthorized access as Moderator.'])->withInput();
+                        }
+                    } else {
+                        if ($user->accountType === 'Admin' || 'admin') {
+                            return redirect()->route('admin.dashboard');
+                        } else {
+                            Auth::logout();
+                            return redirect()->back()->withErrors(['loginError' => 'Unauthorized access as Admin.'])->withInput();
+                        }
+                    }
+                }
+            }
+        }
+        return redirect()->back()->withErrors(['loginError' => 'Invalid username or password.'])->withInput();
+    }
 }
